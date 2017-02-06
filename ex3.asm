90dceb0941e1526979b6b43daf63d05c879098c1
Remario Richards <remariorich@gmail.com>
2017-02-06 13:02:58 +0000

    finished ex3

--- ex3.asm
+++ ex3.asm
@@ -8,14 +8,17 @@ section .data
 	stdout equ 1
 	msg db 'Enter number:',0x0A,
 	mLen equ $ - msg
-	msg2 db 'Result',0x0A,
+	msg2 db 'computation computed.',0x0A,
         mLen2 equ $ - msg2
+	msg3 db 'good job',0x0A,
+        mLen3 equ $ - msg3
+
 
 
 section .bss
 
-input resb 10
-
+input resd  5
+res resd  5
 section .text
 	global _start
 		_start:
@@ -28,30 +31,52 @@ section .text
 			
 			mov eax,sys_read
 			mov ecx,input
-			mov edx,10
+			mov edx,5
 			mov ebx,stdin	
 			int 0x80
-			
 				
-	
-		convertToInt:
+
 			;convert to decimal by sub '0' then store in eax
 			mov eax,[input]
 			sub eax,'0'
+			add eax,2
+
+			;add eax,'0'
+			mov dword [ecx],res
+			
+			;mov ecx,res
+			;mov eax,sys_write
+                        ;mov edx,5
+                        ;mov ebx,stdout
+			;int 0x80
+			;call quit
+
+			
+			
 
 		mov ecx,1 ;start counting from one
-
+	
 		call looper
 		call quit
 			
 		looper:
-		
-			cmp ecx,eax
+			
+			cmp dword [ecx],res
 			jg finished
-			inc eax
+			inc ecx
+			; push eax			
+			push ecx
+                        mov eax,sys_write
+                        mov edx,mLen3
+                        mov ecx,msg3
+                        mov ebx,stdout
+                        int 0x80
+			pop ecx
+			; pop eax
 			jmp looper
 
-		finished:
+
+	       finished:
 			mov eax,sys_write
                         mov edx,mLen2
                         mov ecx,msg2
@@ -59,9 +84,15 @@ section .text
                         int 0x80
 			ret
 
-	
-							
-		quit:		
+	       print:
+			mov eax,sys_write
+                        mov edx,mLen3
+                        mov ecx,msg3
+                        mov ebx,stdout
+                        int 0x80
+                        ret
+					
+	       quit:		
 			mov eax,sys_exit
 			mov ebx,0
 			int 0x80
